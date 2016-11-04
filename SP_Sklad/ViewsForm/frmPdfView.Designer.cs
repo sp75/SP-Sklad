efa7312578b64170f9e8f7b4b4b5e5d3cec3bbdd
sp75 <palamarchuk-sv@mail.ru>
2016-11-04 15:53:46 +0200

    eee

---
+++ SP_Sklad/ViewsForm/frmPdfView.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace SP_Sklad.ViewsForm
+{
+    partial class frmPdfView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pdfViewer1 = new DevExpress.XtraPdfViewer.PdfViewer();
+            this.SuspendLayout();
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.Location = new System.Drawing.Point(33, 51);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.Size = new System.Drawing.Size(628, 421);
+            this.pdfViewer1.TabIndex = 0;
+            // 
+            // frmPdfView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 523);
+            this.Controls.Add(this.pdfViewer1);
+            this.Name = "frmPdfView";
+            this.Text = "frmPdfView";
+            this.Load += new System.EventHandler(this.frmPdfView_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraPdfViewer.PdfViewer pdfViewer1;
+    }
+}
---
+++ SP_Sklad/ViewsForm/frmPdfView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace SP_Sklad.ViewsForm
+{
+    public partial class frmPdfView : DevExpress.XtraEditors.XtraForm
+    {
+        public frmPdfView()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPdfView_Load(object sender, EventArgs e)
+        {
+            //pdfViewer1.LoadDocument(ddddddd)
+            //pdfViewer1.SaveDocument();
+            //pdfViewer1.Print();
+        }
+    }
+}
