50947714b60b6b7c3b0c3ab3d11184b9677da78f
sp75 <br.palamarchuk.sv@gmail.com>
2024-01-18 19:19:39 +0200

    dd

--- SP_Sklad/UserControls/Documents/ucWaybillOut.cs
+++ SP_Sklad/UserControls/Documents/ucWaybillOut.cs
@@ -42,6 +42,7 @@ namespace SP_Sklad.UserControls
 
         private int prev_focused_id = 0;
         private int prev_top_row_index = 0;
+        private int prev_rowHandle = 0;
         private int? find_id { get; set; }
         private bool restore = false;
 
@@ -230,6 +231,8 @@ namespace SP_Sklad.UserControls
 
         public void GetData()
         {
+            prev_rowHandle = WbGridView.FocusedRowHandle;
+
             if (wb_focused_row != null && !find_id.HasValue)
             {
                 prev_top_row_index = WbGridView.TopRowIndex;
@@ -287,6 +290,10 @@ namespace SP_Sklad.UserControls
             if (rowHandle != DevExpress.Data.DataController.OperationInProgress)
             {
                 FocusRow(WbGridView, rowHandle);
+            }
+            else
+            {
+                WbGridView.FocusedRowHandle = prev_rowHandle;
             }
 
             restore = false;
